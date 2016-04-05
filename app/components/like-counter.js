4a3672074f5de9be15fc1a84fc8d4a9c103b7aca
Chris Boone <cboone1337@gmail.com>
2016-04-05 15:30:20 -0500

    buttons move counter

--- app/components/like-counter.js
+++ app/components/like-counter.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  num: 0,
+
+  up(num) {
+    this.set(`num`, this.num + 1);
+  },
+
+  down(num) {
+    this.set(`num`, this.num - 1);
+  },
 });
